a8a1b41d14e9d351fca0006a9ecaae21a62654ab
rajneeshgithub73 <rajneeshrj73@gmail.com>
2023-09-29 05:18:15 +0530

    redux toolkit configuration done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
 
+import { useState, useEffect } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { Outlet } from 'react-router-dom';
+import { Footer, Header } from './components'
+import { login, logout } from './store/authSlice';
 
 function App() {
 
-  return (
-    <>
-      <h1>A Blog with Appwrite.</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }else {
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+  
+  return !loading ? (
+    <div>
+      <div>
+        <Header/>
+        <main>
+          Todo: <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
